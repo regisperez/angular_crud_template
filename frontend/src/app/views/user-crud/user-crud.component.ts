22911a70a684532422a488cc9df313206965ef73
Régis Perez <dev8@redepos.com.br>
2021-05-14 15:29:45 -0300

    Angular Template Crud

---
+++ frontend/src/app/views/user-crud/user-crud.component.ts
@@ -0,0 +1,27 @@
+import { HeaderService } from './../../components/template/header/header.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-user-crud',
+  templateUrl: './user-crud.component.html',
+  styleUrls: ['./user-crud.component.css']
+})
+export class UserCrudComponent implements OnInit {
+
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Usuários',
+      icon: 'verified_user',
+      routeUrl: '/users'
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  navigateToUserCreate(): void {
+    this.router.navigate(['/users/create'])
+  }
+
+}
